fb7bae89b6238e3e1294afe26bde4eb12ef89c65
mrodriguezcartabia <mrodriguezcartabia@gmail.com>
2026-01-12 23:23:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -481,10 +481,14 @@ with herramientas:
                 st.session_state.variable_optimizada = t["tasa_lbl"]
                 st.session_state.resultado_opt = None
     # Botón para optimizar
-    if st.button(t["lbl_hallar"], type="primary", use_container_width=True) and any(p > 0 for p in st.session_state.precios_mercado) and st.session_state.variable_optimizada:
-        st.session_state.resultado_opt = optimizar_parametro(st.session_state.variable_optimizada, st.session_state.precios_mercado, rango_strikes, precio_accion, tasa_r, 
+        btn_hallar = st.button(t["lbl_hallar"], type="primary", use_container_width=True)
+    if btn_hallar and any(p > 0 for p in st.session_state.precios_mercado) and st.session_state.variable_optimizada:
+        with st.spinner(t['msg_loading_2']):
+            st.session_state.resultado_opt = optimizar_parametro(st.session_state.variable_optimizada, st.session_state.precios_mercado, rango_strikes, precio_accion, tasa_r, 
                                                              tiempo_T, sigma, beta, st.session_state.paso_val, param_a)
-    
+        # Otro mensaje temporal de éxito
+        if btn_hallar:
+            st.toast(t["msg_success"])
 
     # Resultado del hallado
     variable = st.session_state.variable_optimizada if st.session_state.variable_optimizada else "x"
